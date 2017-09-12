e54d06d9e29157669506ab649cff00e47da39635
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-12 16:14:09 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Big-Java-Book-Practice
---
+++ src/PracticePrograms/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * The first program
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class HelloWorld
+{
+    public static void main (String args[])
+    {
+        System.out.println("Hello, World!");
+    }
+}
