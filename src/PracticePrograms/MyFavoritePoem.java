8b4dd4db824b0c4e914983800371ba7eab0de071
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 16:35:43 -0400

    My favorite poem exercise

---
+++ src/PracticePrograms/MyFavoritePoem.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class MyFavoritePoem
+{
+    public static void main (String args[])
+    {
+        System.out.println("Invictus");
+        System.out.println("by William Ernest Henley\n");
+        
+        System.out.println("Out of the night that covers me,");
+        System.out.println(" Black as the pit from pole to pole,");
+        System.out.println("I thank whatever gods may be");
+        System.out.println(" For my unconquerable soul.\n");
+        
+        System.out.println("In the fell clutch of circumstance");
+        System.out.println(" I have not winced nor cried aloud.");
+        System.out.println("Under the bludgeonings of chance");
+        System.out.println(" My head is bloody, but unbowed.\n");
+        
+        System.out.println("Beyond this place of wrath and tears");
+        System.out.println(" Looms but the Horror of the shade,");
+        System.out.println("And yet the menace of the years");
+        System.out.println(" Finds and shall find me unafraid.\n");
+        
+        System.out.println("It matters not how strait the gate,");
+        System.out.println(" How charged with punishments the scroll,");
+        System.out.println("I am the master of my fate,");
+        System.out.println(" I am the captain of my soul.");
+    }
+}
