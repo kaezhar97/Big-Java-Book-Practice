5298f130ec79ed624dc3fe0cf8bf42207c027db2
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 16:25:23 -0400

    Finished the animal greeting

---
+++ src/PracticePrograms/AnimalGreeting.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class AnimalGreeting
+{
+    public static void main (String args[])
+    {
+        System.out.println("                        ___________                                                 ");
+        System.out.println("                       |  hey baby |                                               ");
+        System.out.println("\\             /        |___________|                                               ");
+        System.out.println(" +------------+    _____ /                                  ");
+        System.out.println(" |  This is   |----|0 0|                             ");
+        System.out.println(" |  a turtle  |----| - |                                    ");
+        System.out.println(" +------------+                                               ");
+        System.out.println("/              \\                                             ");
+        
+    }
+}
