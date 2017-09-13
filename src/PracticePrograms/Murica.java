7e3bd9f9ce617a45e20394abfd02bf891e726eff
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 16:59:09 -0400

    *eagle screech*

---
+++ src/PracticePrograms/Murica.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class Murica
+{
+    public static void main (String args[])
+    {
+        System.out.println("_________________________________________________________________________");
+        System.out.println("|                       |################################################|");
+        System.out.println("| *   *   *   *   *   * |################################################|");
+        System.out.println("|   *   *   *   *   *   |                                                |");
+        System.out.println("| *   *   *   *   *   * |################################################|");
+        System.out.println("|   *   *   *   *   *   |################################################|");
+        System.out.println("| *   *   *   *   *   * |                                                |");
+        System.out.println("|   *   *   *   *   *   |################################################|");
+        System.out.println("| *   *   *   *   *   * |################################################|");
+        System.out.println("|   *   *   *   *   *   |                                                |");
+        System.out.println("| *   *   *   *   *   * |################################################|");
+        System.out.println("|___*___*___*___*___*___|################################################|");
+        System.out.println("|                                                                        |");
+        System.out.println("|########################################################################|");
+        System.out.println("|########################################################################|");
+        System.out.println("|                                                                        |");
+        System.out.println("|########################################################################|");
+        System.out.println("|########################################################################|");
+        System.out.println("|                                                                        |");
+        System.out.println("|########################################################################|");
+        System.out.println("|########################################################################|");
+     
+        
+  
+        
+    }
+}
