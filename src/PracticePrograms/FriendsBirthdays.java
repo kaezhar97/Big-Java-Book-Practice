010747c20dac806bfe9eba783678c688407bd166
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 17:50:03 -0400

    Finished the friends and birthdays exercise

---
+++ src/PracticePrograms/FriendsBirthdays.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class FriendsBirthdays
+{
+    public static void main (String args[])
+    {
+        System.out.println("My friends and their birthdays");
+        System.out.println("------------------------------");
+        System.out.println("Michael                 Sep 01");
+        System.out.println("Juan                    Aug 09");
+        
+        
+        
+    }
+}
