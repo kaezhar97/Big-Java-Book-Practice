ffc9c218ebdb4b5e7c3d744ecddf923891adad08
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 18:01:51 -0400

    Spanish and English phrases exercise

---
+++ src/PracticePrograms/EnglishAndSpanishPhrases.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class EnglishAndSpanishPhrases
+{
+    public static void main (String args[])
+    {
+        System.out.println("                   English and Spanish phrases");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("Good morning                                           Buenos días");
+        System.out.println("It is a pleasure to meet you                   Placer de conocerte");
+        System.out.println("Please call me tomorrow                   Por favor llámeme mañana");
+        System.out.println("Have a nice day!                               ¡Tenga un buen día!");
+    }
+}
