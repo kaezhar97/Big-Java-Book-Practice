bf597e0fc724d7721aff4c0b533bf256ac8ffeef
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 17:09:29 -0400

    I'm sorry, Dave. I'm afraid I can't do that.

--- src/PracticePrograms/HelloName.java
+++ src/PracticePrograms/HelloName.java
@@ -16,6 +16,9 @@ public class HelloName
     public static void main(String[] args)
     {
         String name = JOptionPane.showInputDialog("What is your name?");
-        System.out.println("Hello "+name);
+        
+        String input = JOptionPane.showInputDialog("Hello " + name + "!\n\nMy name is Hal, what would you like me to do?");
+        
+        System.out.println("I'm sorry, "+name+". I'm afraid I can't do that.");
     }
 }
