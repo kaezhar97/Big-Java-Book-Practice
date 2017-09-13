b45d72a02ca3d68e8ff0e2e76fdda2b9ae21ece3
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 01:30:35 -0400

    Finished the face exercise

---
+++ src/PracticePrograms/Face.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class Face
+{
+    public static void main (String args[])
+    {
+        System.out.println(" ////////// ");
+        System.out.println("/_________\\");
+        System.out.println("|  o   o  |");
+        System.out.println("|    ^    |");
+        System.out.println("|  \\__/   |");
+        System.out.println("\\_________/");
+        
+    }
+    
+}
