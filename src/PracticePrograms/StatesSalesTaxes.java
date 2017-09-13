d33f7127f297e73dc2a22b2375179767c209850c
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 17:54:06 -0400

    Completed a program that displays the sales tax of five states of my choice

---
+++ src/PracticePrograms/StatesSalesTaxes.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class StatesSalesTaxes
+{
+    public static void main (String args[])
+    {
+        System.out.println("Sales Tax Rates");
+        System.out.println("---------------");
+        System.out.println("Florida      6%");
+        System.out.println("Texas     6.25%");
+        System.out.println("Alabama      4%");
+        System.out.println("Virginia   5.3%");
+        System.out.println("Washington 6.5%");
+        
+    }
+}
