9677f052ac2ac1e5f4a235dede9f98668ad8295a
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 17:01:43 -0400

    Finished the Hello your name exercise

---
+++ src/PracticePrograms/HelloYourName.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+import javax.swing.JOptionPane;
+
+public class HelloYourName  
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(null, "Hello, Octavio!");
+    }
+}
