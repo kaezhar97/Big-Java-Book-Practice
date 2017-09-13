abeb8ef962bb3151f09b3c34e756b703d196ee17
Octavio Avila-Cardet <oavil004@fiu.edu>
2017-09-13 01:22:17 -0400

    Finished exercise for displaying my name in morse code

---
+++ src/PracticePrograms/MyNameInMorseCode.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package PracticePrograms;
+
+/**
+ *
+ * @author Octavio
+ */
+public class MyNameInMorseCode
+{
+    public static void main (String args[])
+    {
+        System.out.println("My name is Octavio.\nBelow you can see my name in morse code:");
+        
+        System.out.print("--- ");
+        System.out.print("-.-. ");
+        System.out.print("- ");
+        System.out.print(".- ");
+        System.out.print("...- ");
+        System.out.print(".. ");
+        System.out.println("---");
+    }
+}
